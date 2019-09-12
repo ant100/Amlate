ac014e5dca32edb30b2cb561afd055680e486e5f
Ann Loo <antuanette.loo@outlook.com>
2019-09-12 13:21:12 -0500

    crear pasillo y movimiento de personaje con camara

---
+++ Assets/Scripts/CameraRunner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRunner : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, player.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal") * speed;
+        float v = Input.GetAxis("Vertical") * speed;
+
+        Vector3 vel = rb.velocity;
+        vel.x = v;
+        vel.z = -h;
+        rb.velocity = vel;
+    }
+}
