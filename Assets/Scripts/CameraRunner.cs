f6a3a4fa248062b3f90ad855fcf25fe5ff1e25b8
Ann Loo <antuanette.loo@outlook.com>
2019-09-25 18:10:03 -0500

    mover al personaje por cuadriculas y limitar la camara en los bordes

--- Assets/Scripts/CameraRunner.cs
+++ Assets/Scripts/CameraRunner.cs
@@ -6,10 +6,12 @@ public class CameraRunner : MonoBehaviour
 {
     [SerializeField]
     private Transform player;
+    private float minPosition = 11.0f;
+    private float maxPosition = -43.0f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, player.position.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Clamp(player.position.z, maxPosition, minPosition));
     }
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+   // [SerializeField]
+    private float speed = 4.5f;
+    private bool firstMove;
+    private bool isAtSquare;
+    private float positionX;
+    private float positionZ;
+
+    private float initX;
+    private float initZ;
+
+    private float gridSize = 0.5f;
+    private float movePerFrame;
+    private float gridOffset = 0;
+
+    private enum EnumMovingDir { None, Left,Right, Up, Down};
+    private EnumMovingDir movingDir;
+
+    void Start()
+    {
+        firstMove  = true;
+        isAtSquare = true;
+        movePerFrame = 0.01f * speed;
+    }
+
+    void Update()
+    {
+        movePlayer();
+    }
+
+    void movePlayer()
+    {
+        positionX = transform.position.x;
+        positionZ = transform.position.z;
+
+        if (Input.GetKey("left")) // z+
+        {
+            movingDir = EnumMovingDir.Left;
+            if (firstMove)
+            {
+                firstMove = false;
+                initZ = transform.position.z;
+            }
+
+            positionZ += 0.01f * speed;
+        }
+        else if (Input.GetKey("up")) // x+
+        {
+            movingDir = EnumMovingDir.Up;
+            if (firstMove)
+            {
+                firstMove = false;
+                initX = transform.position.x;
+            }
+
+            positionX += 0.01f * speed;
+        }
+        else if (Input.GetKey("down")) // x- 
+        {
+            movingDir = EnumMovingDir.Down;
+            if (firstMove)
+            {
+                firstMove = false;
+                initX = transform.position.x;
+            }
+
+            positionX -= 0.01f * speed;
+        }
+        else if (Input.GetKey("right")) // z-
+        {
+            movingDir = EnumMovingDir.Right;
+            if (firstMove)
+            {
+                firstMove = false;
+                initZ = transform.position.z;
+            }
+
+            positionZ -= 0.01f * speed;
+        }
+
+        if (Input.GetKeyUp("right") || Input.GetKeyUp("left") || Input.GetKeyUp("up") || Input.GetKeyUp("down"))
+        {
+            float n = 0;
+            float m = 0;
+            firstMove = true;
+
+            switch (movingDir)
+            {
+                case EnumMovingDir.None:
+                    break;
+                case EnumMovingDir.Left:
+                    n = transform.position.z - initZ;
+                    m = n % gridSize;
+                    break;
+                case EnumMovingDir.Right:
+                    n = initZ - transform.position.z;
+                    m = n % gridSize;
+                    break;
+                case EnumMovingDir.Up:
+                    n = transform.position.x - initX;
+                    m = n % gridSize;
+                    break;
+                case EnumMovingDir.Down:
+                    n = initX - transform.position.x;
+                    m = n % gridSize;
+                    break;
+                default:
+                    break;
+            }
+
+            if(m != 0)
+            {
+                isAtSquare = false;
+            }
+            gridOffset = gridSize - m;
+            //Debug.Log("needs to walk " + gridOffset);
+        }
+
+        if(!isAtSquare)
+        {
+            switch (movingDir)
+            {
+                case EnumMovingDir.None:
+                    break;
+                case EnumMovingDir.Left:
+                    positionZ += movePerFrame;
+                    gridOffset = gridOffset - movePerFrame;
+                    //Debug.Log("movePerFrame " + movePerFrame);
+                    //Debug.Log("left to walk " + gridOffset);
+                    if (gridOffset <= 0)
+                    {
+                        isAtSquare = true;
+                    }
+                    break;
+                case EnumMovingDir.Right:
+                    positionZ -= movePerFrame;
+                    gridOffset = gridOffset - movePerFrame;
+                    if(gridOffset <= 0)
+                    {
+                        isAtSquare = true;
+                    }
+                    break;
+                case EnumMovingDir.Up:
+                    positionX += movePerFrame;
+                    gridOffset = gridOffset - movePerFrame;
+                    //Debug.Log("movePerFrame " + movePerFrame);
+                    //Debug.Log("left to walk " + gridOffset);
+                    if (gridOffset <= 0)
+                    {
+                        isAtSquare = true;
+                    }
+                    break;
+                case EnumMovingDir.Down:
+                    positionX -= movePerFrame;
+                    gridOffset = gridOffset - movePerFrame;
+                    if (gridOffset <= 0)
+                    {
+                        isAtSquare = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        transform.position = new Vector3(positionX, transform.position.y, positionZ);
+    }
+}
